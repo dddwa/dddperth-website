80edeac57b8f1e27c7786e38e04aaa29820321b6
Rob Moore <me@robdmoore.id.au>
2018-01-15 22:59:54 +0800

    Use port from environment variables

--- deploy/server.js
+++ deploy/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -19,9 +20,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:' + port)
   })
 })
 .catch((ex) => {
