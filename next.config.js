e4aece1a51bbff7c0ce1e011b9382b1b459b307b
Rob Moore <me@robdmoore.id.au>
2018-01-27 23:32:47 +0800

    Cleaned up dev vs prod dependencies and added analyze step to analyse the web pack bundle https://arunoda.me/blog/ssr-and-server-only-modules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const glob = require('glob')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
 
 module.exports = {
   webpack: (config, { dev }) => {
@@ -32,6 +34,13 @@ module.exports = {
         ]
       }
     )
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
     return config
   },
   exportPathMap: function () {
