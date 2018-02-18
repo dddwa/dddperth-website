01d61360bc2905499146ffd34a3f81ebf651526f
Rob Moore <me@robdmoore.id.au>
2018-02-18 19:11:55 +0800

    Added in real FAQs

--- config/dates.ts
+++ config/dates.ts
@@ -5,10 +5,14 @@ import {Dates as IDates, Conference} from './types';
 export default function getConferenceDates(conference : Conference) : IDates {
   const isComplete = moment(new Date()) > conference.Date.clone().add(1, 'd');
   const isInProgress = moment(new Date()) > conference.Date;
+  const dateDisplayFormat = "ddd Do MMM YYYY";
   return {
-    Display : conference.HideDate ? "TBA" : `${conference.Date.format("ddd Do MMM YYYY")}`,
+    Display : conference.HideDate ? "TBA" : `${conference.Date.format(dateDisplayFormat)}`,
+    DateDisplayFormat : dateDisplayFormat,
+    TimeDisplayFormat : "h:mma",
     IsComplete : isComplete,
     IsInProgress : isInProgress && !isComplete,
-    HasNotStarted : !isInProgress && !isComplete
+    HasNotStarted : !isInProgress && !isComplete,
+    RegistrationOpen : moment(new Date()) > conference.RegistrationOpenFrom && !conference.IsSoldOut
   };
 }
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,62 +1,27 @@
 import * as React from 'react'
+import {Fragment} from 'react';
 import Page from '../layouts/main';
 import {withPageMetadata} from '../components/global/withPageMetadata';
 import Link from 'next/link'
 import Conference from '../config/conference';
 import { PanelGroup, Panel } from 'react-bootstrap';
+import Faqs from '../config/faqs';
 
 export default withPageMetadata(() =>
   <Page title="FAQs" description={"Frequently asked questions for the "+Conference.Name+" conference."}>
     <h1>FAQs</h1>
     <PanelGroup accordion className="accordion" id="faqs-accordion">
-      <Panel eventKey="1">
+      {Faqs.map((faq, i) => <Panel eventKey={i} key={i}>
         <Panel.Heading>
-          <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
+          <Panel.Title toggle>{faq.Question}</Panel.Title>
         </Panel.Heading>
         <Panel.Body collapsible>
-          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-          dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-          moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-          butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-          aesthetic synth nesciunt you probably haven't heard of them accusamus
-          labore sustainable VHS.</p>
+          {faq.Answer
+            ? <p>{faq.Answer}</p>
+            : <Fragment>{faq.AnswerWithoutParagraph}</Fragment>
+          }
         </Panel.Body>
-      </Panel>
-      <Panel eventKey="2">
-        <Panel.Heading>
-          <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body collapsible>
-          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-          dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-          moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-          butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-          aesthetic synth nesciunt you probably haven't heard of them accusamus
-          labore sustainable VHS.</p>
-        </Panel.Body>
-      </Panel>
-      <Panel eventKey="3">
-        <Panel.Heading>
-          <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body collapsible>
-          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-          dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-          moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-          butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-          aesthetic synth nesciunt you probably haven't heard of them accusamus
-          labore sustainable VHS.</p>
-        </Panel.Body>
-      </Panel>
+      </Panel>)}
     </PanelGroup>
   </Page>
 );
