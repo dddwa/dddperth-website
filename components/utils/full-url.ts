3312dc9d0c6ba6bd991b2c2fc87d0c4e26fa4af1
Rob Moore <me@robdmoore.id.au>
2018-02-26 18:40:50 +0800

    Fixing up url code

--- components/utils/full-url.ts
+++ components/utils/full-url.ts
@@ -1,17 +1,17 @@
 import * as http from 'http';
 
 export function getUrlFromNodeRequest(request : http.IncomingMessage) {
-  var secure = (request.connection as any).encrypted || request.headers['x-forwarded-proto'] === 'https';
+  const secure = (request.connection as any).encrypted || request.headers['x-forwarded-proto'] === 'https';
   return 'http' + (secure ? 's' : '') + '://' + request.headers.host + request.url;
 }
 
 export function getUrlFromWindow(window : Window) {
-  var origin = window.location.origin ?
+  const origin = window.location.origin ?
     window.location.origin :
     window.location.protocol + '//' + window.location.host;
 
-  var url = [
-    window.location.origin.replace(/\/$/, ''),
+  const url = [
+    origin.replace(/\/$/, ''),
     window.location.pathname.replace(/^\//, '')
   ];
 
