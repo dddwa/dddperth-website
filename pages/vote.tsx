ffd1768d2d616a6158646bac7e522a9bb342e81e
Rob Moore <robm@dddperth.com>
2018-06-07 01:50:28 +0800

    Added server-side rendering

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch'
 import Link from 'next/link'
 import Router from 'next/router'
 import * as React from 'react'
@@ -9,14 +10,18 @@ import getConferenceDates from '../config/dates'
 import { Session } from '../config/types'
 import Page from '../layouts/main'
 
+interface VoteProps extends WithPageMetadataProps {
+  sessions?: Session[]
+}
+
 interface VoteState {
   sessions?: Session[]
   isLoading: boolean
   isError: boolean
 }
 
-class VotePage extends React.Component<WithPageMetadataProps, VoteState> {
-  static getInitialProps({ res }) {
+class VotePage extends React.Component<VoteProps, VoteState> {
+  static async getInitialProps({ req, res }) {
     const dates = getConferenceDates(Conference, dateTimeProvider.now())
     if (!dates.VotingOpen) {
       if (res) {
@@ -29,10 +34,38 @@ class VotePage extends React.Component<WithPageMetadataProps, VoteState> {
         Router.replace('/')
       }
     }
+    if (req) {
+      const sessionSubmissionsUrl = process.env.GET_SUBMISSIONS_URL
+      const secure = (req.connection as any).encrypted || req.headers['x-forwarded-proto'] === 'https'
+      const url = sessionSubmissionsUrl.startsWith('/')
+        ? 'http' + (secure ? 's' : '') + '://' + req.headers.host + sessionSubmissionsUrl
+        : sessionSubmissionsUrl
+      const response = await fetch(url)
+
+      if (response.status !== 200) {
+        return {}
+      }
+
+      const body = await response.json()
+      return { sessions: body }
+    }
     return {}
   }
 
   componentWillMount() {
+    this.setState({
+      isError: false,
+      isLoading: !this.props.sessions,
+      sessions: this.props.sessions || [],
+    })
+  }
+
+  componentDidMount() {
+    if (this.props.sessions) {
+      this.setSessions(this.props.sessions)
+      return
+    }
+
     const that = this
     this.setState({
       isError: false,
@@ -46,43 +79,7 @@ class VotePage extends React.Component<WithPageMetadataProps, VoteState> {
         return response.json()
       })
       .then(body => {
-        // if the client does not support local storage then just set session state
-        if (!localStorage) {
-          this.setState({
-            isLoading: false,
-            sessions: body as Session[],
-          })
-        } else {
-          const sessions = body as Session[]
-          const orderings = localStorage.getItem('ddd-voting-session-order')
-
-          // if previous ordering data has not been persisted in local storage
-          if (orderings == null) {
-            const ids = JSON.stringify(sessions.map(({ Id }) => Id)) // Randomizing will be done in backend API
-
-            localStorage.setItem('ddd-voting-session-order', ids)
-
-            this.setState({
-              isLoading: false,
-              sessions,
-            })
-          } else {
-            // if previous ordering data has been persisted then apply this and override API response ordering
-            const indicies = new Map<string, number>(JSON.parse(orderings).map((id, index) => [id, index]))
-            const preordered = sessions
-              .map(session => ({
-                index: indicies.get(session.Id) || 0,
-                session,
-              }))
-              .sort(({ index: first }, { index: second }) => first - second)
-              .map(({ session }) => session) as Session[]
-
-            this.setState({
-              isLoading: false,
-              sessions: preordered,
-            })
-          }
-        }
+        this.setSessions(body as Session[])
       })
       .catch(error => {
         that.setState({ isError: true, isLoading: false })
@@ -91,6 +88,45 @@ class VotePage extends React.Component<WithPageMetadataProps, VoteState> {
           console.error('Error loading sessions', error)
         }
       })
+  }
+
+  setSessions(sessions: Session[]) {
+    // if the client does not support local storage then just set session state
+    if (!localStorage) {
+      this.setState({
+        isLoading: false,
+        sessions,
+      })
+    } else {
+      const orderings = localStorage.getItem('ddd-voting-session-order')
+
+      // if previous ordering data has not been persisted in local storage
+      if (orderings == null) {
+        const ids = JSON.stringify(sessions.map(({ Id }) => Id)) // Randomizing will be done in backend API
+
+        localStorage.setItem('ddd-voting-session-order', ids)
+
+        this.setState({
+          isLoading: false,
+          sessions,
+        })
+      } else {
+        // if previous ordering data has been persisted then apply this and override API response ordering
+        const indicies = new Map<string, number>(JSON.parse(orderings).map((id, index) => [id, index]))
+        const preordered = sessions
+          .map(session => ({
+            index: indicies.get(session.Id) || 0,
+            session,
+          }))
+          .sort(({ index: first }, { index: second }) => first - second)
+          .map(({ session }) => session) as Session[]
+
+        this.setState({
+          isLoading: false,
+          sessions: preordered,
+        })
+      }
+    }
   }
 
   render() {
