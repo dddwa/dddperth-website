b3ba954db9efa11fc70d1768766ec707983180b9
Rob Moore <robm@dddperth.com>
2018-06-09 22:58:18 +0800

    Allowed generic update of conference properties from environment variables :O

--- components/global/withPageMetadata.tsx
+++ components/global/withPageMetadata.tsx
@@ -1,3 +1,5 @@
+import { isMoment } from 'moment'
+import * as moment from 'moment'
 import * as React from 'react'
 import Conference from '../../config/conference'
 import getConferenceDates from '../../config/dates'
@@ -8,11 +10,17 @@ import { withCurrentDate, WithCurrentDateProps } from '../withCurrentDate'
 
 // https://dev.to/danhomola/react-higher-order-components-in-typescript-made-simple
 
+export interface ConferenceConfig {
+  key: string
+  value: string
+}
+
 export interface AppConfig {
   getSubmissionsUrl: string
   instrumentationKey: string
   submitVoteUrl: string
   testingMode: boolean
+  conferenceConfig: ConferenceConfig[]
 }
 declare global {
   interface Window {
@@ -55,8 +63,13 @@ const withPageMetadata = <TOriginalProps extends {}>(
       const pagePath = context.req ? context.pathname : null
       const pageUrl = context.req ? url.getUrlFromNodeRequest(context.req) : null
 
+      const conferenceConfig = Object.keys(process.env)
+        .filter(key => key.match(/^Conference_/))
+        .map(key => ({ key: key.replace(/^Conference_/, ''), value: process.env[key] }))
+
       const appConfig = context.req
         ? {
+            conferenceConfig,
             getSubmissionsUrl: process.env.GET_SUBMISSIONS_URL,
             instrumentationKey: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
             submitVoteUrl: process.env.SUBMIT_VOTE_URL,
@@ -86,13 +99,34 @@ const withPageMetadata = <TOriginalProps extends {}>(
     }
 
     render() {
+      const updateConference = (conference: IConference, conferenceConfig: ConferenceConfig[]) => {
+        conferenceConfig.forEach(({ key, value }) => {
+          if (conference[key] instanceof String) {
+            conference[key] = value
+          }
+          if (conference[key] instanceof Boolean) {
+            conference[key] = value === 'true'
+          }
+          if (conference[key] instanceof Number) {
+            conference[key] = Number.parseInt(value)
+          }
+          if (isMoment(conference[key])) {
+            conference[key] = moment.parseZone(value)
+          }
+        })
+        return conference
+      }
+
       return (
         <WrappedComponent
           pageMetadata={{
             appConfig: this.props.appConfig,
-            conference: Conference,
+            conference: updateConference(Conference, this.props.appConfig.conferenceConfig),
             currentDate: this.props.currentDate,
-            dates: getConferenceDates(Conference, this.props.currentDate),
+            dates: getConferenceDates(
+              updateConference(Conference, this.props.appConfig.conferenceConfig),
+              this.props.currentDate,
+            ),
             pagePath: this.pagePath,
             pageUrl: this.pageUrl,
           }}
