b29d3cee3ea2d9334467f19bf2f6620e7a6421d2
Rob Moore <robm@dddperth.com>
2018-06-12 09:48:58 +0800

    Added note on voting page to set community expectations

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -238,6 +238,23 @@ class VotePage extends React.Component<VoteProps, VoteState> {
 
           <hr />
 
+          <p className="alert alert-warning">
+            <strong>Please note:</strong> Our expectation of the community, <strong>and you as a voter</strong>, is that
+            you will only lodge one vote and you will vote for a set of talks that make up <em>your</em> perfect agenda
+            regardless of your friends talks.<br />
+            <br />If you know who submitted any of the sessions we ask that you do not discuss that with anyone or post
+            it on social media so we can retain a level playing field for all submitters through anonymity. We
+            definitely want you to post/talk about the conference and encouraging others to vote though so spread the
+            word.
+            <br />
+            <br /> If we follow this as a community then we can be fair to the submitters who have put time, effort and
+            courage into crafting the amazing session proposals below. If you have any questions please{' '}
+            <a href={'mailto:' + this.props.pageMetadata.conference.ContactEmail}>contact us</a>.
+            <br />
+            <br />
+            Thanks!<br />&lt;3 DDD Perth team
+          </p>
+
           {this.state.isLoading && <p>Loading sessions...</p>}
           {this.state.isError && (
             <p className="alert alert-danger">
