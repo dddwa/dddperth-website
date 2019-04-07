c63e47c43f9452ba8319edec5d8ede6ef9173541
Anton Ball <antonball@gmail.com>
2019-04-07 11:58:29 +0800

    Fixes #53 Adds aria-expanded property to menu button

--- components/global/nav.tsx
+++ components/global/nav.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import { Nav as BSNav, Navbar, NavItem } from 'react-bootstrap'
 import { MenuItem } from '../../config/types'
 
@@ -14,43 +14,35 @@ const isFirstBranchMatched = (pagePath: string, href: string) => {
   return href === sub
 }
 
-class Nav extends React.Component<NavArgs> {
-  constructor(props: NavArgs) {
-    super(props)
-  }
+export const Nav: React.FC<NavArgs> = props => {
+  const [expanded, setExpanded] = useState(false)
 
-  render() {
-    return (
-      <Navbar className="main">
-        <Navbar.Header>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <BSNav>
-            {this.props.menu.map(item => (
-              <Fragment key={item.href}>
-                <Link href={item.href} passHref>
-                  <NavItem active={isFirstBranchMatched(this.props.pagePath, item.href)}>
-                    {!item.external && item.name}
-                    {item.external && (
-                      <span>
-                        <span aria-hidden="true">
-                          {item.name}
-                          &nbsp;
-                          <i className="fa fa-external-link" />
-                        </span>
-                        <span className="sr-only">{item.name}, will open in a new window</span>
-                      </span>
-                    )}
-                  </NavItem>
-                </Link>
-              </Fragment>
-            ))}
-          </BSNav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
+  return (
+    <Navbar className="main" onToggle={(val: boolean) => setExpanded(val)}>
+      <Navbar.Header>
+        <Navbar.Toggle aria-expanded={expanded} />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <BSNav>
+          {props.menu.map(item => (
+            <Link key={item.href} href={item.href} passHref>
+              <NavItem active={isFirstBranchMatched(props.pagePath, item.href)}>
+                {!item.external && item.name}
+                {item.external && (
+                  <span>
+                    <span aria-hidden="true">
+                      {item.name}
+                      &nbsp;
+                      <i className="fa fa-external-link" />
+                    </span>
+                    <span className="sr-only">{item.name}, will open in a new window</span>
+                  </span>
+                )}
+              </NavItem>
+            </Link>
+          ))}
+        </BSNav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
-
-export default Nav
