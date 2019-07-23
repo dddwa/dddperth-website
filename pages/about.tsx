1fa1c262e807123efd2b71b1e686aa62bd79ca13
Andrew Best <andrew.best@readify.net>
2019-07-23 20:20:51 +0930

    Updates background for non index pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import React from 'react'
 import { SafeLink } from '../components/global/safeLink'
 import withPageMetadata, { WithPageMetadataProps } from '../components/global/withPageMetadata'
