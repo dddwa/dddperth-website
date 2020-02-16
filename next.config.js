fc12f72668d0bf9a73a82aa70674c2389fec2e2a
Anton Ball <antonball@gmail.com>
2020-02-16 21:01:24 +0800

    Cleaning next config
    Tweaking the next config for more modern bundle-analyzer and removing
    es6-shim. We shouldn't need it as Next handles polyfills for us.

--- next.config.js
+++ next.config.js
@@ -1,30 +1,10 @@
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const {
-  BundleAnalyzerPlugin
-} = require('webpack-bundle-analyzer')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass(
     withBundleAnalyzer({
-      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      webpack: (config, {
-        dev
-      }) => {
-        if (!dev) {
-          const originalEntry = config.entry;
-          config.entry = async () => {
-            const entries = await originalEntry();
-
-            if (entries['main.js']) {
-              entries['main.js'].unshift('./public/static/scripts/es6-shim.js');
-            }
-
-            return entries;
-          };
-        }
-        return config;
-      },
       poweredByHeader: false
     })
 )
