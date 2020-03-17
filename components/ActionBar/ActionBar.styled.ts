dcddb1168023258f2da0b6dde8390d158a6d58a9
Anton Ball <antonball@gmail.com>
2020-03-17 20:53:07 +0800

    Adds the ActionBar component
    ActionBar sits below the main navigation. I suspect it will adjust more
    with the dates but for now it matches the design as is.
    
    Thought this would be added on index which is why that's refactored but
    it now lives in template but decided to keep the refactor anyway.

---
+++ components/ActionBar/ActionBar.styled.ts
@@ -0,0 +1,33 @@
+import styled from '../utils/styles/theme'
+import { breakpoint, breakpointMax } from '../utils/styles/breakpoints'
+
+export const StyledActionBarContainer = styled('div')(({ theme }) => ({
+  backgroundColor: theme.colors.grey100,
+  borderBottom: `1px solid ${theme.colors.grey300}`,
+
+  '> div': {
+    display: 'flex',
+    flexGrow: 1,
+    alignItems: 'center',
+    maxWidth: `120ch`,
+    margin: '0 auto',
+  },
+
+  p: {
+    display: 'none',
+    flexGrow: 1,
+    textAlign: 'center',
+    color: theme.colors.grey800,
+
+    [breakpoint('lg')]: {
+      display: 'block',
+    },
+  },
+
+  a: {
+    [breakpointMax('lg')]: {
+      flexGrow: 1,
+      textAlign: 'center',
+    },
+  },
+}))
