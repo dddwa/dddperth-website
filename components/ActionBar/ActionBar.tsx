8882ef5eaf7ab779a82b19e87a4ce5b285ed7424
Anton Ball <antonball@gmail.com>
2020-03-19 20:32:19 +0800

    Use actions util for ActionBar

--- components/ActionBar/ActionBar.tsx
+++ components/ActionBar/ActionBar.tsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import { ButtonAnchor } from '../global/Button/Button'
 import { PageMetadata } from '../global/withPageMetadata'
-import getConferenceDates from '../../config/dates'
-import dateTimeProvider from '../utils/dateTimeProvider'
 import { StyledActionBarContainer } from './ActionBar.styled'
+import getConferenceActions from '../../config/actions'
 
 interface ActionBarProps {
   metadata: PageMetadata
 }
 
 export const ActionBar: React.FC<ActionBarProps> = ({ metadata }) => {
-  const { AcceptingPresentations } = getConferenceDates(metadata.conference, dateTimeProvider.now())
+  const [primaryAction, secondaryAction] = getConferenceActions(metadata.conference, metadata.dates)
 
   return (
     <StyledActionBarContainer>
       <div>
         <p>DDD Perth is an inclusive conference for the Perth tech community.</p>
-        {AcceptingPresentations && (
-          <ButtonAnchor href="/cfp" kind="secondary" size="lg">
-            Submit talk
+        {secondaryAction && (
+          <ButtonAnchor href={secondaryAction.Url} kind="secondary" size="lg">
+            {secondaryAction.Title}
           </ButtonAnchor>
         )}
-        <ButtonAnchor href="/tickets" kind="primary" size="lg">
-          Buy Tickets
-        </ButtonAnchor>
+        {primaryAction && (
+          <ButtonAnchor href={primaryAction.Url} kind="primary" size="lg">
+            {primaryAction.Title}
+          </ButtonAnchor>
+        )}
       </div>
     </StyledActionBarContainer>
   )
