661bf32b3685b734094ed985ac54d3b0e9295dc9
Anton Ball <antonball@gmail.com>
2020-05-04 14:36:42 +0800

    Updates footer design
    Updates the footer design as part of the site redesign. This is missing
    a few of the social icons. Have reached out to get those sorted but we
    can merge this and update that later.

--- components/global/Footer/footerNav.tsx
+++ components/global/Footer/footerNav.tsx
@@ -2,35 +2,29 @@ import Link from 'next/link'
 import React, { StatelessComponent } from 'react'
 import { MenuItem } from '../../../config/types'
 import { SafeLink } from '../safeLink'
+import { StyledFooterNav } from './Footer.styled'
 
 interface FooterNavArgs {
   menu: MenuItem[]
 }
 
-const FooterNav: StatelessComponent<FooterNavArgs> = ({ menu }) => (
-  <nav className="foot-nav">
+export const FooterNav: StatelessComponent<FooterNavArgs> = ({ menu }) => (
+  <StyledFooterNav>
     <ul>
-      {menu.map(item => (
+      {menu.map((item) => (
         <li key={item.href}>
           {!item.external && (
-            <Link href={item.href} replace={!item.external}>
+            <Link href={item.href}>
               <a>{item.name}</a>
             </Link>
           )}
           {item.external && (
-            <SafeLink href={item.href} target="_blank">
-              <span aria-hidden="true">
-                {item.name}
-                &nbsp;
-                <i className="fa fa-external-link" />
-              </span>
-              <span className="sr-only">{item.name}, will open in a new window</span>
+            <SafeLink href={item.href} target="_blank" aria-label={`${item.name}, will open in a new window`}>
+              {item.name}
             </SafeLink>
           )}
         </li>
       ))}
     </ul>
-  </nav>
+  </StyledFooterNav>
 )
-
-export default FooterNav
