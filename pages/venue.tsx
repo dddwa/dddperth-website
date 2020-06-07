d390fc504e7421fae654fdc6bee8c8d46514f090
Anton Ball <antonball@gmail.com>
2020-06-07 12:21:13 +0800

    Updates venue page
    Updates the venue page. Lost the icons after the removal of the icon
    font, so simplified the layout. Can always revist if the design team
    have input. Removed the google maps component as it seemed overkill.
    Replaced instead with the hero component that shows PCEC and the
    relevant date.

--- pages/venue.tsx
+++ pages/venue.tsx
@@ -1,134 +1,77 @@
-import GoogleMapReact from 'google-map-react'
 import Error from 'next/error'
 import React from 'react'
 import { SafeLink } from '../components/global/safeLink'
 import withPageMetadata, { WithPageMetadataProps } from '../components/global/withPageMetadata'
 import Conference from '../config/conference'
 import { Main } from '../layouts/main'
+import { NextPage } from 'next'
+import { StyledPara } from '../components/global/text'
 
-class VenuePage extends React.Component<WithPageMetadataProps> {
-  static getInitialProps({ res }) {
-    if (Conference.HideVenue && res) {
-      res.statusCode = 404
-    }
-    return {}
-  }
-  handleGoogleMapApi(google: any) {
-    google.map.setMapTypeId(google.maps.MapTypeId.SATELLITE)
-  }
-  render() {
-    const conference = this.props.pageMetadata.conference
+const VenuePage: NextPage<WithPageMetadataProps> = ({ pageMetadata }) => {
+  const conference = pageMetadata.conference
 
-    if (conference.HideVenue) {
-      return <Error statusCode={404} />
-    }
-    return (
-      <Main metadata={this.props.pageMetadata} title="Venue" description={`About the ${conference.Name} venue.`}>
-        <div className="container">
-          <h1>Venue</h1>
-          <p>
-            {conference.Name} will be held at {conference.Venue.Name} at{' '}
-            <SafeLink
-              href={
-                'https://www.google.com.au/maps/place/' +
-                encodeURIComponent(conference.Venue.Name + ', ' + conference.Venue.Address)
-              }
-              target="_blank"
-            >
-              {conference.Venue.Address}
-            </SafeLink>
-            .
-          </p>
-        </div>
-        <div id="map" aria-hidden>
-          <div id="map-view">
-            <GoogleMapReact
-              bootstrapURLKeys={{
-                key: 'AIzaSyACDtKFE3lyOOmHpMeUoJsYqaVY2fcaa9o',
-              }}
-              options={{
-                mapTypeControl: false,
-                minZoomOverride: false,
-                panControl: false,
-                scrollwheel: false,
-                zoomControl: false,
-              }}
-              center={{
-                lat: conference.Venue.Latitude,
-                lng: conference.Venue.Longitude,
-              }}
-              zoom={17}
-              yesIWantToUseGoogleMapApiInternals={true}
-              onGoogleApiLoaded={this.handleGoogleMapApi.bind(this)}
-            />
-          </div>
-          <div id="map-overlay">
-            <h2>{conference.Venue.Name}</h2>
-            <h3>{conference.Venue.Address}</h3>
-          </div>
-        </div>
-        <section className="right-sidebar" id="travelinfo">
-          <div className="container directions equal-heights">
-            {conference.Venue.Car && (
-              <div className="col">
-                <div className="top">
-                  <i className="fa fa-car" aria-hidden="true" />
-                  <h3>Car</h3>
-                </div>
-                <div className="txt">
-                  <p>{conference.Venue.Car}</p>
-                </div>
-              </div>
-            )}
-            {conference.Venue.Train && (
-              <div className="col">
-                <div className="top">
-                  <i className="fa fa-train" aria-hidden="true" />
-                  <h3>Train</h3>
-                </div>
-                <div className="txt">
-                  <p>{conference.Venue.Train}</p>
-                </div>
-              </div>
-            )}
-            {conference.Venue.Tram && (
-              <div className="col">
-                <div className="top">
-                  <i className="fa fa-subway" aria-hidden="true" />
-                  <h3>Tram</h3>
-                </div>
-                <div className="txt">
-                  <p>{conference.Venue.Tram}</p>
-                </div>
-              </div>
-            )}
-            {conference.Venue.Bus && (
-              <div className="col">
-                <div className="top">
-                  <i className="fa fa-bus" aria-hidden="true" />
-                  <h3>Bus</h3>
-                </div>
-                <div className="txt">
-                  <p>{conference.Venue.Bus}</p>
-                </div>
-              </div>
-            )}
-            {conference.Venue.Accommodation && (
-              <div className="col">
-                <div className="top">
-                  <i className="fa fa-building" aria-hidden="true" />
-                  <h3>Accommodation</h3>
-                </div>
-                <div className="txt">
-                  <p>{conference.Venue.Accommodation}</p>
-                </div>
-              </div>
-            )}
-          </div>
-        </section>
-      </Main>
-    )
+  if (conference.HideVenue) {
+    return <Error statusCode={404} />
   }
+
+  return (
+    <Main metadata={pageMetadata} title="Venue" description={`About the ${conference.Name} venue.`} showHero={true}>
+      <h1>Venue</h1>
+      <StyledPara>
+        {conference.Name} will be held at {conference.Venue.Name} at{' '}
+        <SafeLink
+          href={
+            'https://www.google.com.au/maps/place/' +
+            encodeURIComponent(conference.Venue.Name + ', ' + conference.Venue.Address)
+          }
+          target="_blank"
+        >
+          {conference.Venue.Address}
+        </SafeLink>
+        .
+      </StyledPara>
+
+      <section>
+        {conference.Venue.Car && (
+          <React.Fragment>
+            <h3>Car</h3>
+            <StyledPara>{conference.Venue.Car}</StyledPara>
+          </React.Fragment>
+        )}
+        {conference.Venue.Train && (
+          <React.Fragment>
+            <h3>Train</h3>
+            <StyledPara>{conference.Venue.Train}</StyledPara>
+          </React.Fragment>
+        )}
+        {conference.Venue.Tram && (
+          <React.Fragment>
+            <h3>Tram</h3>
+            <StyledPara>{conference.Venue.Tram}</StyledPara>
+          </React.Fragment>
+        )}
+        {conference.Venue.Bus && (
+          <React.Fragment>
+            <h3>Bus</h3>
+            <StyledPara>{conference.Venue.Bus}</StyledPara>
+          </React.Fragment>
+        )}
+        {conference.Venue.Accommodation && (
+          <React.Fragment>
+            <h3>Accommodation</h3>
+            <StyledPara>{conference.Venue.Accommodation}</StyledPara>
+          </React.Fragment>
+        )}
+      </section>
+    </Main>
+  )
+}
+
+VenuePage.getInitialProps = ({ res }) => {
+  if (Conference.HideVenue && res) {
+    res.statusCode = 404
+  }
+  return {} as WithPageMetadataProps
 }
 
 export default withPageMetadata(VenuePage)
