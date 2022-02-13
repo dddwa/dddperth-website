068d2563b45817d6a54ce6687a7dec4178b879ed
Anton <antonball@gmail.com>
2022-02-13 09:38:30 +0800

    Cypress progress commit

---
+++ cypress/integration/cfp.ts
@@ -0,0 +1,13 @@
+describe('anonymous calculator;', () => {
+  it('can make calculations', () => {
+    // cy.clock()
+    const now = new Date('2021-04-19T08:00:00').getTime()
+
+    // cy.clock(now)
+    // cy.wait('trying', { timeout: 10000 })
+    // cy.wait(10000)
+    cy
+      .visit('/')
+      .pause()
+  })
+})
