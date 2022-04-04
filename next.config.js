1831622ef3b6898547bfe140fec7132217082227
Rob Moore <robm@dddperth.com>
2022-04-05 02:08:12 +0800

    Removing bundle analyser require from production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
+const config = {
   poweredByHeader: false,
   experimental: {
     outputStandalone: process.env.STANDALONE_BUILD === 'true',
   },
-})
+}
+
+if (process.env.ANALYZE) {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true,
+  })
+  module.exports = withBundleAnalyzer(config)
+} else {
+  module.exports = config
+}
