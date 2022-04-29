d2e4848144b49699d2d0af843670cfeef5660f71
Anton <antonball@gmail.com>
2022-04-29 19:47:13 +0800

    Update OG Image
    The OG Image is showing the 2021 logo which is a weird experience for
    users.
    
    Swap to the generic DDDPerth logo until such time as we have a more
    suitable image available.

--- components/global/meta.tsx
+++ components/global/meta.tsx
@@ -20,7 +20,7 @@ declare global {
 const getTitle = (title: string, date: Date, name: string, showDate: boolean) =>
   `${title !== 'Home' ? title + ' - ' : ''}${name}${showDate ? ` | ${format(date, 'do MMMM yyyy')}` : ''}`
 
-export const Meta = ({ pageTitle, pageDescription, pageImage = '/static/images/logo-2021.png' }: MetaArgs) => {
+export const Meta = ({ pageTitle, pageDescription, pageImage = '/static/images/logo.png' }: MetaArgs) => {
   const { conference, appConfig, dates } = useConfig()
   const { pathname } = useRouter()
   const [title, setTitle] = React.useState('')
