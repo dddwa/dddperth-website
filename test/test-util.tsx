54c80d3104cd68b1d486a0a64755a2b011229c58
Ben Lowry <bplowry@gmail.com>
2022-06-01 21:40:45 +0800

    Remove cache provider in tests too. They still pass

--- test/test-util.tsx
+++ test/test-util.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import { render as rtlRender, RenderResult } from '@testing-library/react'
-import { CacheProvider, ThemeProvider } from '@emotion/react'
-import createCache from '@emotion/cache'
+import { ThemeProvider } from '@emotion/react'
 import { theme } from 'components/utils/styles/theme'
 import { ConfigProvider } from 'Context/Config'
 
 function render(ui: React.ReactElement, { ...options } = {}): RenderResult {
-  const emotionCache = createCache({
-    key: 'test-cache',
-  })
-  emotionCache.compat = true
-
   function Wrapper({ children }) {
     return (
       <ConfigProvider>
-        <CacheProvider value={emotionCache}>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </CacheProvider>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </ConfigProvider>
     )
   }
