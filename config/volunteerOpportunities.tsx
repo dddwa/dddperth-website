3f52e262ca3e132d5d0d4272ab7538264cd2e603
Sham Chukoury <eleusis@gmail.com>
2022-06-17 01:19:25 +0800

    Update volunteer opportunity descriptions

--- config/volunteerOpportunities.tsx
+++ config/volunteerOpportunities.tsx
@@ -5,17 +5,7 @@ export default function getVolunteerOpportunities(): VolunteerOpportunity[] {
   const opportunities: VolunteerOpportunity[] = []
 
   opportunities.push({
-    RoleTitle: "CFP Mentor/CFP Proofing",
-    Description: (
-      <Fragment>
-        Throughout the CFP process, we provide ad-hoc mentoring to those who ask for it. This is usually just emailing
-        and providing feedback on proposals, helping them finesse their submission.
-      </Fragment>
-    )
-  })
-
-  opportunities.push({
-    RoleTitle: "Speaker Mentor",
+    RoleTitle: "👩‍🏫 Speaker Mentor",
     Description: (
       <Fragment>
         Once talks are chosen, we run training sessions for speakers to give them advice and tips on putting together
@@ -26,37 +16,33 @@ export default function getVolunteerOpportunities(): VolunteerOpportunity[] {
   })
 
   opportunities.push({
-    RoleTitle: "Speaker Liaison",
+    RoleTitle: "🎁 Speaker Gift Elves",
     Description: (
       <Fragment>
-        On the conference day, point of contact for speakers and checking that they're OK and have everything they need.
-        e.g. Is the speaker there, do they need anything, do they know where they need to go, do they have everything, etc.
+        Organising, planning, assembling and packing speaker gifts. This is a role that requires some time in the lead
+        up to the conference. The gifts will be sourced already and there will be some time elected (probably in the
+        evening) where a small group will come together with the committee member leading this initiative to assemble
+        the packs. Important note, this is not a standalone role and will be coupled with an "on the day" operational role.
+        Please advise if there are any roles that you specifcially do not want to be assigned. Or if you have another
+        area of interest in an operational role on the day that would be a great help too!
       </Fragment>
     )
   })
 
   opportunities.push({
-    RoleTitle: "CFP Workshop Mentoring",
+    RoleTitle: "🎤 Room MC's",
     Description: (
       <Fragment>
-        We need mentors to help at the event, this is usually just prompting new speakers about what they could talk
-        about and listening to draft proposals, giving some light and supportive feedback on improvements.
+        Room MC, Coordinator, host.. Whatever the right name is. The person who stands in front of the audience in a
+        selected room and makes that room HAPPEN! You are responsible for ensuring each session starts and finishes
+        on time, the speakers are mic'd up and everone behaves themselves. So it goes without saying  - you need to be
+        comfortable speaking in front of large audiences, good at timekeeping and able think on your feet.
       </Fragment>
     )
   })
 
   opportunities.push({
-    RoleTitle: "Speaker Gift Elves",
-    Description: "Organising, planning, assembling and packing speaker gifts"
-  })
-
-  opportunities.push({
-    RoleTitle: "Room MC's",
-    Description: "TBD"
-  })
-
-  opportunities.push({
-    RoleTitle: "Social Buzz",
+    RoleTitle: "💬 Social Buzz",
     Description: (
       <Fragment>
         On the day, one volunteer per room whose sole job is to take a couple of photos and publish social posts about
@@ -66,48 +52,60 @@ export default function getVolunteerOpportunities(): VolunteerOpportunity[] {
   })
 
   opportunities.push({
-    RoleTitle: "General Runners/Ushers",
-    Description: "TBD"
-  })
-
-  opportunities.push({
-    RoleTitle: "Admission",
+    RoleTitle: "🏃‍♀️ General Runners/Ushers",
     Description: (
       <Fragment>
-        Admission volunteers need be available for a 6am start on the day of the conference day and be able to take direction
-        from a team lead with regards to the effective flow of equipment, supplies and setup from the loading dock to
-        their intended location. It's important that they are physically fit, free from any injury that may hinder their ability
-        to undertake light to medium lifting and repetitive tasks.
+        General assistance and working under the guidance of the team lead on the day. Ushers simply make stuff happen
+        and are paramount to the effective running of the conference while things are in flight. From herding large
+        groups of people between rooms, enforcing room capacity numbers and bringing a thing from one side of the
+        venue to another becasue someone needs it - prepare for the unexpected!
       </Fragment>
     )
   })
 
   opportunities.push({
-    RoleTitle: "Bump In/Out",
-    Description: "TBD"
-  })
-
-  opportunities.push({
-    RoleTitle: "Event Management",
-    Description: "TBD"
-  })
-
-  opportunities.push({
-    RoleTitle: "Photography",
+    RoleTitle: "🎟 Admission",
     Description: (
       <Fragment>
-        Photographers need their own camera body, flash and lens, but will be provided an SD card that must be returned to
-        AV solutions Director at the end of the conference day.
+        Responsible for working under the direction of a team lead to efficiently check delegates into the venue/conference
+        using the nominated digial check-in tool. You will be required to attend a pre-event training session to ensure
+        familiarity and your ability to work in a high pressure environment during the check-in period. The idea here
+        is that this group of people are the same as those from Bump In thus will need to be available from 6am
       </Fragment>
     )
   })
 
   opportunities.push({
-    RoleTitle: "Concierge",
+    RoleTitle: "↔ Bump In/Out",
+    Description: (
+      <Fragment>
+        Requires the Volunteer to be available for a 6am start on the day of the event and able to take direction
+        from a team lead with regards to the effective flow of equipment, supplies and setup from the loading dock
+        to their intended location. Thus it is important volunteers are physically fit, free form any injury that may
+        hinder them from undertaking light to medium lifting and repetitive tasks.
+      </Fragment>
+    )
+  })
+
+  opportunities.push({
+    RoleTitle: "📷 Photography ",
+    Description: (
+      <Fragment>
+        PHOTOGRAPHERS! as you know, or if you don't yet you are about to find out. DDD LOVE creating a social buzz
+        and having swathes of pictorial evidence of the fun our attendees, speakers and volunteers have on the day.
+        So if you self-identify as a photographer, and have a super expensive snazzy camera PLEASE REGISTER YOUR INTEREST!
+      </Fragment>
+    )
+  })
+
+  opportunities.push({
+    RoleTitle: "🤝 Concierge",
     Description: (
       <Fragment>
         Works with Gold level sponsors to ensure they maximise the value derived from their partnership with DDD.
-        This includes help with designing their booth experience and assistance on the conference day.
+        This includes help with designing their booth experience and assistance on the conference day. This role is
+        required in the lead-up to the event and it would be great to hear if there are some operational roles that
+        are required on the day that you may also be interested in.
       </Fragment>
     )
   })
