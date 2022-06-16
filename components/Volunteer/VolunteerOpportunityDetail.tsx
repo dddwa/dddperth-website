0d7d6a552381563ef974f760cb48c3b1a64999da
Sham Chukoury <eleusis@gmail.com>
2022-06-17 01:30:20 +0800

    Update styling of volunteer opportunity list to align with FAQ expandable styling - Also add CTA button to every opportunity to jump to sign up form

--- components/Volunteer/VolunteerOpportunityDetail.tsx
+++ components/Volunteer/VolunteerOpportunityDetail.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { VolunteerOpportunity } from 'config/types'
 import { StyledDetails, StyledDescriptionBody, StyledRoleTitle } from './VolunteerOpportunityDetail.styled'
+import { Action } from 'config/types'
+import { ActionButton } from 'components/actionButton'
 
 export interface VolunteerOpportunityProps {
   opportunity: VolunteerOpportunity
 }
 
+{
+  var volunteer_form: Action = {
+    Title: "Sign Up",
+    Url: "#volunteer_form",
+    Category: null
+  }
+}
+
 export const VolunteerOpportunityDetail = ({ opportunity }: VolunteerOpportunityProps) => (
   <StyledDetails>
     <StyledRoleTitle>
-      {opportunity.RoleTitle}
+      <span>{opportunity.RoleTitle}</span>
     </StyledRoleTitle>
     <StyledDescriptionBody>
       {opportunity.Description}
+      <div className='btn'>
+        <ActionButton action={volunteer_form} />
+      </div>
     </StyledDescriptionBody>
   </StyledDetails>
 )
--- components/Volunteer/VolunteerOpportunityList.tsx
+++ components/Volunteer/VolunteerOpportunityList.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { VolunteerOpportunity } from 'config/types'
 import { VolunteerOpportunityDetail } from './VolunteerOpportunityDetail'
-import { StyledList } from './VolunteerOpportunityDetail.styled'
 
 interface VolunteerOpportunityListProps {
   opportunities: VolunteerOpportunity[]
 }
 
 export const VolunteerOpportunityList = ({ opportunities }: VolunteerOpportunityListProps) => (
-  <StyledList>
+  <div>
     {opportunities.map((opportunity, i) => {
-      return <li key={i}><VolunteerOpportunityDetail opportunity={opportunity} /></li>
+      return <VolunteerOpportunityDetail key={i} opportunity={opportunity} />
     })}
-  </StyledList>
+  </div>
 )
