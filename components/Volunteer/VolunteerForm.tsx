628826a8a0c1b180be668a35e18e8ec6e6ca9366
Sham Chukoury <eleusis@gmail.com>
2022-07-07 17:57:16 +0800

    (Minor) Fix lint errors

--- components/Volunteer/VolunteerForm.tsx
+++ components/Volunteer/VolunteerForm.tsx
@@ -1,16 +1,16 @@
 import { useConfig } from 'Context/Config'
 import Script from 'next/script';
 
-declare var SmFormSettings: any;
+declare const SmFormSettings: any;
 
 export const VolunteerForm = () => {
-  const { conference } = useConfig()
+  const { conference } = useConfig();
 
   if (conference.VolunteerSubmissionFormId) {
     return (
       <div id="volunteer_form">
         <Script strategy='afterInteractive' type='text/javascript' id='loadFormScript' src='https://webforms.salesmate.io/webforms.js' onLoad={() => {
-          var WEBFORM_DATA = {
+          const WEBFORM_DATA = {
             formId: `${conference.VolunteerSubmissionFormId}`,
             divId: '_sm_webform_',
             linkName: 'webforms.salesmate.io'
