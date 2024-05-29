7cacd7f2c02d268028ba9a35121edce84b9da054
Andrew Best <andrew.a.best@gmail.com>
2024-05-29 11:30:23 +0930

    Cleanup

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { StyledHero, StyledCredit, StyledConferenceDetails } from './Hero.styled'
-import { SafeLink } from 'components/global/safeLink'
+import { StyledHero, StyledConferenceDetails } from './Hero.styled'
 import { formatISO } from 'date-fns'
 import { useConfig } from 'Context/Config'
 
