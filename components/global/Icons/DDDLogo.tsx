0910f947a58efc40f5fe904774583615179bad38
Andrew Best <andrew.a.best@gmail.com>
2024-05-29 11:07:44 +0930

    Adjusts logo

--- components/global/Icons/DDDLogo.tsx
+++ components/global/Icons/DDDLogo.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import Image from 'next/image'
 
 export const DDDLogo = () => (
-  <Image src="/static/images/adelaide/logo.svg" alt="DDD Adelaide logo" width={280} height={80} style={{ marginTop: 10, marginBottom: 10 }} />
+  <Image src="/static/images/adelaide/logo.svg" alt="DDD Adelaide logo" width={280} height={70} style={{ marginTop: 10, marginBottom: 10 }} />
 )
