340a1f395e6adb4264b977f96722755d76266bc7
Jake Ginnivan <jake@ginnivan.net>
2024-06-13 19:18:23 +0800

    Update workshop link

--- config/workshops.tsx
+++ config/workshops.tsx
@@ -4,12 +4,12 @@ export const workshops: {
   bookingLink?: string
   dates: ImportantDate[]
 } = {
-  bookingLink: 'https://ti.to/dddperth/virtual-cfp-workshop-2024',
+  bookingLink: 'https://ti.to/dddperth/f2f-cfp-workshop-2024',
   dates: [
     {
       Type: 'cfp-workshop',
-      Date: new Date('2024-06-13T17:30:00+08:00'),
-      Description: 'CFP & Bio Workshop - (Online & In Person)',
+      Date: new Date('2024-06-25T17:30:00+08:00'),
+      Description: 'CFP & Bio Workshop - (In Person)',
     },
   ],
 }
